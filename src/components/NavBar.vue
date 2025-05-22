14b45a83b4ea239319118f492e150bb4cf68ba29
Edward <114949726+Ejz9@users.noreply.github.com>
2025-05-22 13:11:51 -0500

    Update.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/projects">Projects</router-link>
+    <router-link to="/contact">Contact</router-link>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,17 @@
 </script>
 
 <template>
-  <div><h1>Home Page</h1><p>Welcome to my portfolio!</p></div>
+  <div>
+    <h1>Hello World!</h1>
+    <h2>Edward Zurakowski</h2>
+    <h3>Full Stack Tinkerer</h3>
+    <p>
+      I am an aspiring professional with an interest in Computer Science and Cybersecurity.
+      Currently I am a student in my senior year attending the University of Wisconsin-Whitewater. I am pursuing a Bachelor's of Science in Computer Science with a minor in Cybersecurity.
+      I have a keen interest in coding software, and building tools to help achieve specific goals. Also, I am an enthusiast for Open-Source and everything under its' umbrella.
+    </p>
+
+  </div>
 </template>
 
 <style scoped>
